5f05bffb9926ab5e40f8d180174a5b0c36f417fc
spring.tao <32430149@qq.com>
2019-05-15 16:37:14 +0800

    add ServiceBase class that produce resultxml

--- dlnasample/dlna/servicemgr.py
+++ dlnasample/dlna/servicemgr.py
@@ -18,59 +18,49 @@ class ServiceMgr:
     def unregistService(self, serviceName):
         self.services.remove(serviceName)
 
-class AVTransportService:
+class ServiceBase:
+    def __init__(self,servicename):
+        self.servicename = servicename
+    
+    def resultXml(self, action, **kwargs):
+        resultstr = ''
+        for key, value in kwargs.items():
+            itemstr = '<{0}>{1}</{0}>\n'.format(key, value)
+            resultstr += itemstr
+        
+        resultxmlformat = """<?xml version="1.0"?>
+    <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+        <SOAP-ENV:Body>
+            <m:{cmd}Response xmlns:m="{service}">
+            {result}
+            </m:{cmd}Response>
+        </SOAP-ENV:Body>
+    </SOAP-ENV:Envelope>"""
+        return resultxmlformat.format(cmd=action,result=resultstr,service=self.servicename)
+
+class AVTransportService(ServiceBase):
     def __init__(self, servicemgr):
+        ServiceBase.__init__(self,'urn:schemas-upnp-org:service:AVTransport:1')
         servicemgr.registService('urn:schemas-upnp-org:service:AVTransport:1',self)
     
     def SetAVTransportURI(self, **kwargs):
         print '[AVTransportService.SetAVTransportURI] kwargs : ', kwargs
-        return """<?xml version="1.0"?>
-    <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-        <SOAP-ENV:Body>
-            <m:SetAVTransportURIResponse xmlns:m="urn:schemas-upnp-org:service:AVTransport:1">
-            </m:SetAVTransportURIResponse>
-        </SOAP-ENV:Body>
-    </SOAP-ENV:Envelope>"""
+        return self.resultXml('SetAVTransportURI',**{})
 
     def Play(self, **kwargs):
         print '[AVTransportService.Play] kwargs : ', kwargs
-        return """<?xml version="1.0"?>
-    <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-        <SOAP-ENV:Body>
-            <m:PlayResponse xmlns:m="urn:schemas-upnp-org:service:AVTransport:1">
-            </m:PlayResponse>
-        </SOAP-ENV:Body>
-    </SOAP-ENV:Envelope>"""
+        return self.resultXml('Play',**{})
 
     def Stop(self, **kwargs):
         print '[AVTransportService.Stop] kwargs : ', kwargs
-        return """<?xml version="1.0"?>
-    <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-        <SOAP-ENV:Body>
-            <m:StopResponse xmlns:m="urn:schemas-upnp-org:service:AVTransport:1">
-            </m:StopResponse>
-        </SOAP-ENV:Body>
-    </SOAP-ENV:Envelope>"""
+        return self.resultXml('Stop',**{})
 
     def Pause(self, **kwargs):
         print '[AVTransportService.Pause] kwargs : ', kwargs
-        return """<?xml version="1.0"?>
-    <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-        <SOAP-ENV:Body>
-            <m:PauseResponse xmlns:m="urn:schemas-upnp-org:service:AVTransport:1">
-            </m:PauseResponse>
-        </SOAP-ENV:Body>
-    </SOAP-ENV:Envelope>"""
+        return self.resultXml('Pause',**{})
 
     def GetCurrentTransportActions(self, **kwargs):
         print '[AVTransportService.GetCurrentTransportActions] kwargs : ', kwargs
-        return """<?xml version="1.0"?>
-    <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-        <SOAP-ENV:Body>
-            <m:GetCurrentTransportActionsResponse xmlns:m="urn:schemas-upnp-org:service:AVTransport:1">
-                <Actions>Play</Actions>
-            </m:GetCurrentTransportActionsResponse>
-        </SOAP-ENV:Body>
-    </SOAP-ENV:Envelope>"""
+        return self.resultXml('GetCurrentTransportActions',**{"Actions":"Play"})
 
 serviceManager = ServiceMgr()
